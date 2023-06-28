340f5bb620908a94b028fc1c117160984a2ab3db
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-06-28 19:14:52 +0300

    Added httpx.AsyncClient

--- functions/github_links.py
+++ functions/github_links.py
@@ -6,9 +6,11 @@ A. Функция принимает в качестве аргумента на
    Функция должна обработать полученные ссылки и вывести в консоль названия
    самих гит-проектов. Стоит рассмотреть защиту от ссылок "вне формата".
 """
+import asyncio
 import os
 import sys
 
+import aiohttp
 import httpx
 
 if __package__:
@@ -22,7 +24,7 @@ DOMAIN = 'github.com'
 
 
 @decorators.input
-def __get_test_data():
+def __get_test_data() -> tuple[str]:
     return (
         'https://github.com/alexpro2022/cargo_transportation-FastAPI.git',
         'https://github.com/miguelgrinberg/Flask-SocketIO',
@@ -32,34 +34,41 @@ def __get_test_data():
         'https://github.com/tiangolo/fastapi.git',
         'https://github.com/tiangolo/uvicorn-gunicorn-fastapi-docker.git',
         'https://ya.ru',
+        'asdr.com',
     )
 
 
-def is_valid(url):
-    return httpx.get(url).status_code in (200, 301)
+async def get_valid(urls: tuple[str]) -> tuple[str]:
+    async with httpx.AsyncClient() as client:
+        for url in urls:
+            try:
+                response = await client.get(url)
+            except ValueError:
+                continue
+            if response.status_code in (200, 301):
+                yield url
 
 
-def get_github_project(url: str) -> str | None:
+async def get_github_project(url: str) -> str | None:
     try:
         _, _, domain, _, *remain = url.split('/')
     except ValueError:
         return None
-    if domain != DOMAIN or not is_valid(url):
+    if domain != DOMAIN:
         return None
     return remain[0].split('.')[0]
 
 
-@decorators.timer
-def get_github_projects(urls):
-    return [project for project in [get_github_project(url) for url in urls]
+@decorators.atimer
+async def get_github_projects(urls):
+    return [project for project in [await get_github_project(url) async for url in get_valid(urls)]
             if project is not None]
 
 
 @decorators.timer
 @decorators.output(title=__doc__)
 def main():
-    return get_github_projects(__get_test_data())
-
+    return asyncio.run(get_github_projects(__get_test_data()))
 
 if __name__ == '__main__':
     main()
