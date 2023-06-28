1103f78c9ba19ce3ba0e185b3c11873a7e052708
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-06-28 22:37:52 +0300

    Compare two ways of requesting the urls

--- functions/github_links.py
+++ functions/github_links.py
@@ -37,17 +37,6 @@ def __get_test_data() -> tuple[str]:
     )
 
 
-async def get_valid(urls: tuple[str]) -> tuple[str]:
-    async with httpx.AsyncClient() as client:
-        for url in urls:
-            try:
-                response = await client.get(url)
-                if response.status_code in (200, 301):
-                    yield url
-            except ValueError:
-                pass              
-
-
 def get_github_project(url: str) -> str | None:
     try:
         _, _, domain, _, *remain = url.split('/')
@@ -58,17 +47,61 @@ def get_github_project(url: str) -> str | None:
     return remain[0].split('.')[0]
 
 
+# <=== async/await ===>
+async def _get_valid(urls: tuple[str]) -> tuple[str]:
+    async with httpx.AsyncClient() as client:
+        for url in urls:
+            try:
+                response = await client.get(url)
+                if response.status_code in (200, 301, 302):
+                    yield url
+            except ValueError:
+                pass  
+
+
 @decorators.atimer
-async def get_github_projects(urls):
+async def _get_github_projects(urls):
     return [project for project in [get_github_project(url)
-            async for url in get_valid(urls)]
+            async for url in _get_valid(urls)]
             if project is not None]
+# </=== async/await ===>
+
+
+# <=== asyncio.gather() ===> approx. 3-4 times faster 
+async def get_url(client, url):
+    try:
+        response = await client.get(url)
+    except (httpx.UnsupportedProtocol, ValueError):
+        return None
+    return url if response.status_code in (200, 301, 302) else None
+
+
+async def gather_valid(urls: tuple[str]) -> tuple[str]:
+    async with httpx.AsyncClient() as client:
+        coros = [get_url(client, url) for url in urls]
+        return [url for url in await asyncio.gather(*coros)
+                if url is not None]
+
+
+@decorators.atimer
+async def gather_github_projects(urls):
+    return [project for project in [get_github_project(url)
+            for url in await gather_valid(urls)]
+            if project is not None]
+# </=== asyncio.gather() ===>
+
+
+async def run_together():
+    res1 = await _get_github_projects(__get_test_data())
+    res2 = await gather_github_projects(__get_test_data())
+    assert res1 == res2
+    return res2
 
 
 @decorators.timer
 @decorators.output(title=__doc__)
 def main():
-    return asyncio.run(get_github_projects(__get_test_data()))
+    return asyncio.run(run_together())
 
 
 if __name__ == '__main__':
